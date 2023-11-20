b0056f4e6591c0ebf2ad6bdf894a3d15a87d946b
owais qureshi <qureshiowais91@gmail.com>
2023-11-20 15:25:27 +0530

    init

---
+++ BackEndTask/crud/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+console.log(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI);
+const userSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  username: String,
+  email: String,
+  address: {
+    street: String,
+    suite: String,
+    city: String,
+    zipcode: String,
+    geo: {
+      lat: String,
+      lng: String,
+    },
+  },
+  phone: String,
+  website: String,
+  company: {
+    name: String,
+    catchPhrase: String,
+    bs: String,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.json());
+
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.put('/users', async (req, res) => {
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.put('/usersdelete', async (req, res) => {
+  try {
+    await User.deleteOne({ id: req.params.id });
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
