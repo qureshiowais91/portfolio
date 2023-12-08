eb76287fa8c8706697d2f92b79afa84903f96ce9
owais qureshi <qureshiowais91@gmail.com>
2023-12-08 08:48:26 +0530

    init

---
+++ FrontEndTask/todoWithRedux/src/App.jsx
@@ -0,0 +1,15 @@
+ import AddTodo from './component/AddTodo'
+ import Todos from './component/Todo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ FrontEndTask/todoWithRedux/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import  App from './App'
+import {store} from './store/store'
+
+
+// As of React 18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
