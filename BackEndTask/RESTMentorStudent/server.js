4a0b0ae7daf4b9e2034590c69c2720667450c58c
owais qureshi <qureshiowais91@gmail.com>
2023-12-23 11:20:46 +0530

    cluster

--- BackEndTask/RESTMentorStudent/server.js
+++ BackEndTask/RESTMentorStudent/server.js
@@ -4,16 +4,29 @@ import * as mentor from "./router/mentor.js"
 import * as student from "./router/student.js"
 import { connectDB } from "./util/connectDB.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import cluster from 'cluster';
+import os from 'os';
+const numCpu = os.cpus().length;
+
 
 dotenv.config();
-
 const app = express();
-connectDB()
-app.use(express.json())
+connectDB();
+
+app.use(express.json());
 app.use("/mentor", mentor.router);
 app.use("/student", student.router);
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("connected");
-})
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCpu; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(3000, () => {
+    console.log(`connected,${process.pid}`);
+  })
+}
+
+
