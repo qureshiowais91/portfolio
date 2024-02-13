871dc8617cbff3e1681c214937e430b06d86616a
owais qureshi <qureshiowais91@gmail.com>
2024-02-13 16:27:37 +0530

    api live

--- FullStackTask/URL Shortener Application/src/API.js
+++ FullStackTask/URL Shortener Application/src/API.js
@@ -1,9 +1,9 @@
-// export const API = {
-//     USER_URL: 'https://urlshort-65ao.onrender.com/user',
-//     URL_URL: 'https://urlshort-65ao.onrender.com/url'
-//   };
-
-  export const API = {
-    USER_URL: 'http://127.0.0.1:3001/user',
-    URL_URL: 'http://127.0.0.1:3001/url'
+export const API = {
+    USER_URL: 'https://urlshort-65ao.onrender.com/user',
+    URL_URL: 'https://urlshort-65ao.onrender.com/url'
   };
+
+  // export const API = {
+  //   USER_URL: 'http://127.0.0.1:3001/user',
+  //   URL_URL: 'http://127.0.0.1:3001/url'
+  // };
